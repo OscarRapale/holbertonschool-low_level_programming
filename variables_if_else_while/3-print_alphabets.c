65b8f82d033f169ef73c27b7c95f81920701725d
OscarRapale <you@example.com>
2024-02-20 13:35:49 -0400

    alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main function
+ * Printing alphabets lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+  char letter;
+
+  for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+  for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	
+  putchar('\n');
+
+  return(0);
+
+}i
