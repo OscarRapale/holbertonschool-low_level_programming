9b4e7248cdaa6d24e8c2b493afb47dbbb1216193
OscarRapale <you@example.com>
2024-02-19 22:16:18 -0400

    quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
- *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
