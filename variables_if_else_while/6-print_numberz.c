2bf465e1fa74c8abdc05741cb2fe4fa01438a2d6
OscarRapale <you@example.com>
2024-02-20 16:12:45 -0400

    print numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Printing single digits numbers of base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
+
