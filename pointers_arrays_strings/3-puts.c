df8ee722c073f227f4ea3ac815b23034c28e9c76
OscarRapale <you@example.com>
2024-02-26 19:30:56 -0400

    puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,19 +3,8 @@
 
 /**
  * _puts - prints a string to stdout
- * int main(void) - declaring and calling _puts
  * @str: pointer to the string to print
  */
-void _puts(char *str);
-
-int main(void)
-{
-	char str[] = "Hello, world";
-
-	_puts(str);
-
-	return (0);
-}
 void _puts(char *str)
 {
 	int i = 0;
