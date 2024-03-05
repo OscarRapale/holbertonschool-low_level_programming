be76a0f7b3c653a07958d9f2f02cb6fc5dfb6a19
OscarRapale <you@example.com>
2024-03-05 13:15:29 -0400

    fix

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -8,8 +8,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	printf("%s\n", argc - 1);
+	printf("%s\n", argv[0]);
 	return (0);
 }
