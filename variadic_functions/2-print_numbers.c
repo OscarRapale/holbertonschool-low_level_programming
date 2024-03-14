ab6aaacda474ced4ed1f86308064f8c77c3cfdbe
OscarRapale <you@example.com>
2024-03-14 18:35:12 -0400

    print numbers

--- variadic_functions/2-print_numbers.c
+++ variadic_functions/2-print_numbers.c
@@ -8,21 +8,21 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list list;
+	va_list args;
 
-	va_start(list, n);
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 		if (!separator)
-			printf("%d", va_arg(list, int));
+			printf("%d", va_arg(args, int));
 		else if (separator && i == 0)
-			printf("%d", va_arg(list, int));
+			printf("%d", va_arg(args, int));
 		else
-			printf("%s%d", separator, va_arg(list, int));
+			printf("%s%d", separator, va_arg(args, int));
 	}
 
-	va_end(list);
+	va_end(args);
 
 	printf("\n");
 }
