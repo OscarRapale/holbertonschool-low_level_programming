c267856599e9a6514a0c325fed813cc6bc58c317
OscarRapale <you@example.com>
2024-03-14 23:45:05 -0400

    hello world asm

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,18 +1,9 @@
-global _start
-
-section .text:
-
-_start:
-  mov eax, 0x4
-  mov ebx, 1
-  mov ecx, message
-  mov edx, message_length
-  int 0x80
-
-  mov eax, 0x1
-  mov ebx, 0
-  int 0x80
-
-section .data:
-  message: db "Hello, World", 0xa
-  message_length equ $-message
+          global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, World\n`,0
