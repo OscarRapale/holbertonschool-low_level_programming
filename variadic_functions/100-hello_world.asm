df3a7fec10b46bfa94492a087a5b0c605106507a
OscarRapale <you@example.com>
2024-03-14 23:49:26 -0400

    hello world asm

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,9 +1,21 @@
-          global    main
-          extern    printf
-main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-format: db `Hello, World\n`,0
+section .data
+    hello_world db 'Hello, World!', 10 ; String to print
+
+section .text
+    global _start
+
+_start:
+
+    mov rax, 1              ; System call number for write
+    mov rdi, 1              ; File descriptor: 1 (stdout)
+    mov rsi, hello_world    ; Pointer to the string to print
+    mov rdx, 14             ; Length of the string
+
+    ; Invoke the write syscall
+    syscall
+
+    ; Exit the program
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
